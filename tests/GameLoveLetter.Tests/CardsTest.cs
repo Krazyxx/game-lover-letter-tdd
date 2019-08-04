b832885a9436cc65882c5c1ccc0ba262f9bca192
Krazyxx <jimmy.gouraud@hotmail.fr>
2019-08-04 16:35:10 +0200

    Simplification of cards testing

--- tests/GameLoveLetter.Tests/CardsTest.cs
+++ tests/GameLoveLetter.Tests/CardsTest.cs
@@ -1,18 +1,42 @@
-﻿using Xunit;
+﻿using System.Collections;
+using System.Collections.Generic;
+using Xunit;
 
 namespace GameLoveLetter.Tests
 {
+
+	public class CardTestData : IEnumerable<object[]>
+	{
+		private readonly List<object[]> _data = new List<object[]>
+		{
+			new object[] { new Guard(),		"Guard",	1 },
+			new object[] { new Priest(),	"Priest",	2 },
+			new object[] { new Baron(),		"Baron",	3 },
+			new object[] { new Handmaid(),	"Handmaid", 4 },
+			new object[] { new Prince(),	"Prince",	5 },
+			new object[] { new King(),		"King",		6 },
+			new object[] { new Countess(),	"Countess", 7 },
+			new object[] { new Princess(),	"Princess", 8 }
+		};
+
+		public IEnumerator<object[]> GetEnumerator()
+		{
+			return _data.GetEnumerator();
+		}
+
+		IEnumerator IEnumerable.GetEnumerator()
+		{
+			return GetEnumerator();
+		}
+	}
+
 	public class CardsTest
 	{
-		#region Check card's properties
-
-		[Fact]
-		public void CheckNameAndStrengthOfGuardCard()
+		[Theory]
+		[ClassData(typeof(CardTestData))]
+		public void CheckNameAndStrengthOfCards(Card card, string cardName, int cardStrength)
 		{
 			// A
-			var card = new Guard();
-			var cardName = "Guard";
-			var cardStrength = 1;
 
 			// A
 
@@ -20,113 +44,5 @@ namespace GameLoveLetter.Tests
 			Assert.Equal(cardName, card.Name);
 			Assert.Equal(cardStrength, card.Strength);
 		}
-
-		[Fact]
-		public void CheckNameAndStrengthOfPriestCard()
-		{
-			// A
-			var card = new Priest();
-			var cardName = "Priest";
-			var cardStrength = 2;
-
-			// A
-
-			// A
-			Assert.Equal(cardName, card.Name);
-			Assert.Equal(cardStrength, card.Strength);
-		}
-
-		[Fact]
-		public void CheckNameAndStrengthOfBaronCard()
-		{
-			// A
-			var card = new Baron();
-			var cardName = "Baron";
-			var cardStrength = 3;
-
-			// A
-
-			// A
-			Assert.Equal(cardName, card.Name);
-			Assert.Equal(cardStrength, card.Strength);
-		}
-
-		[Fact]
-		public void CheckNameAndStrengthOfHandmaidCard()
-		{
-			// A
-			var card = new Handmaid();
-			var cardName = "Handmaid";
-			var cardStrength = 4;
-
-			// A
-
-			// A
-			Assert.Equal(cardName, card.Name);
-			Assert.Equal(cardStrength, card.Strength);
-		}
-
-		[Fact]
-		public void CheckNameAndStrengthOfPrinceCard()
-		{
-			// A
-			var card = new Prince();
-			var cardPrinceName = "Prince";
-			var cardPrinceStrength = 5;
-
-			// A
-
-			// A
-			Assert.Equal(cardPrinceName, card.Name);
-			Assert.Equal(cardPrinceStrength, card.Strength);
-		}
-
-		[Fact]
-		public void CheckNameAndStrengthOfKingCard()
-		{
-			// A
-			var card = new King();
-			var cardName = "King";
-			var cardStrength = 6;
-
-			// A
-
-			// A
-			Assert.Equal(cardName, card.Name);
-			Assert.Equal(cardStrength, card.Strength);
-		}
-
-		[Fact]
-		public void CheckNameAndStrengthOfCountessCard()
-		{
-			// A
-			var card = new Countess();
-			var cardName = "Countess";
-			var cardStrength = 7;
-
-			// A
-
-			// A
-			Assert.Equal(cardName, card.Name);
-			Assert.Equal(cardStrength, card.Strength);
-		}
-
-		[Fact]
-		public void CheckNameAndStrengthOfPrincessCard()
-		{
-			// A
-			var card = new Princess();
-			var cardName = "Princess";
-			var cardStrength = 8;
-
-			// A
-			
-			// A
-			Assert.Equal(cardName, card.Name);
-			Assert.Equal(cardStrength, card.Strength);
-		}
-
-		#endregion // Check card's properties
-
 	}
 }
